06cb4e9806c155b73c03ca8165006b8f0b13f9a6
Leon Kaspers <117840392+leonkaspers@users.noreply.github.com>
2025-05-27 00:56:03 +0200

    delete uneccesary files

--- Frontend/meet-add-on/samples/hello-world-next-js/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/meet/hello-world-next-js',
-  distDir: '../dist/hello-world-next-js',
-  output: 'export',
-};
-
-export default nextConfig;
--- Frontend/meet-add-on/samples/hello-world-next-js/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-
-  return (
-    <>
-      <div>This page left intentionally blank. See /sidepanel and /mainstage</div>
-    </>
-  )
-}
